d6e0ebd6593ce368ed23715a92bfae374690b31b
SkYfy1 <festik.tawer@gmail.com>
2025-04-16 00:47:38 +0300

    feature(Header) - created initial header

---
+++ components/Header.tsx
@@ -0,0 +1,36 @@
+import Image from "next/image";
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="flex justify-between items-center lg:pt-2 lg:pb-5">
+      <div className="flex gap-12 items-center">
+        <Image src="/svg/lofofrntr.svg" width={170} height={60} alt="logo" />
+        <div className="flex justify-between items-center gap-12">
+          <input
+            type="text"
+            placeholder="Names, categories..."
+            className="bg-gray-200 rounded-full px-6 py-2"
+          />
+          <ul className="flex gap-12 items-center">
+            <li>Shop</li>
+            <li>Stories</li>
+            <li>Promotions</li>
+            <li>Room</li>
+            <li>Specials</li>
+          </ul>
+        </div>
+      </div>
+      <div className="flex gap-3 items-center">
+        <Image src="/svg/userIcon.svg" width={30} height={30} alt="user icon" />
+        <Image src="/svg/basketIcon.svg" width={30} height={30} alt="basket" />
+        <select name="En" id="En">
+          <option value="En">En</option>
+          <option value="Fr">En</option>
+        </select>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
