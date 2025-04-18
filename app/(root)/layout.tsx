50f55304b85af7f4b2656f67227157ec053328ec
SkYfy1 <festik.tawer@gmail.com>
2025-04-18 04:02:59 +0300

    initial(root) - page + layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,7 +1,15 @@
-import React from "react";
+import Header from "@/components/Header";
+import React, { ReactNode } from "react";
 
-const Layout = () => {
-  return <main className="min-h-screen w-screen"></main>;
+const Layout = ({ children }: { children: ReactNode }) => {
+  return (
+    <main className="root-container">
+      <div className="mx-auto w-full pt-2 pb-2 text-black">
+        <Header />
+        <div className="mt-16">{children}</div>
+      </div>
+    </main>
+  );
 };
 
 export default Layout;
