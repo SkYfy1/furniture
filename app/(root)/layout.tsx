45c01117eba0d93a8a25e66c3c9b08c51efc32a5
SkYfy1 <festik.tawer@gmail.com>
2025-04-19 03:04:34 +0300

    refactor(layout/page) - added footer in layout/main page template

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,12 +1,14 @@
+import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import React, { ReactNode } from "react";
 
 const Layout = ({ children }: { children: ReactNode }) => {
   return (
     <main className="root-container">
-      <div className="mx-auto w-full pt-2 pb-2 text-black">
+      <div className="pt-2 pb-2 text-black">
         <Header />
         <div className="mt-16">{children}</div>
+        <Footer />
       </div>
     </main>
   );
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,28 +4,35 @@ import SaleOverview from "@/components/SaleOverview";
 
 export default function Home() {
   return (
-    <>
+    <div className="grid-cols-custom grid gap-5 gap-y-5 my-5">
       <SaleOverview />
-      <section className="grid-cols-custom grid gap-5 gap-y-5 my-5">
-        <CardSection />
-        <div className="pt-24 col-start-2 col-end-5 h-full w-full flex-col flex gap-8">
-          <div className="flex gap-2 flex-col">
-            <h1 className="text-3xl font-bold">
-              To Plant a Garden is To Believe in Tomorrow
-            </h1>
-            <p>
-              Our plants are sure to brighten up your home (and your future).
-            </p>
-          </div>
-          <div className="flex gap-3 w-full justify-between">
-            <Product />
-            <Product />
-            <Product />
-            <Product />
-            <Product />
-          </div>
+      <CardSection />
+      <section className="pt-24 col-start-2 col-end-5 h-full w-full flex-col flex gap-8">
+        <div className="flex gap-2 flex-col">
+          <h1 className="text-3xl font-bold">
+            To Plant a Garden is To Believe in Tomorrow
+          </h1>
+          <p>Our plants are sure to brighten up your home (and your future).</p>
+        </div>
+        <div className="flex gap-3 w-full justify-between">
+          <Product />
+          <Product />
+          <Product />
+          <Product />
+          <Product />
         </div>
       </section>
-    </>
+      <section className="w-full bg-green-50 pt-24 col-span-5">
+        <div className="container pb-10">
+          <h1 className="text-3xl font-bold">Outlet</h1>
+          <p>Enjoy discounts on the following:</p>
+        </div>
+        <div className="flex gap-3 w-full container">
+          <Product />
+          <Product />
+          <Product />
+        </div>
+      </section>
+    </div>
   );
 }
