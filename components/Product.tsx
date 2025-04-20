e5c5fc20e3624547b11c899f54c122b007af4304
SkYfy1 <festik.tawer@gmail.com>
2025-04-20 03:44:39 +0300

    refactor(components/Product) - square image

--- components/Product.tsx
+++ components/Product.tsx
@@ -4,14 +4,15 @@ import React from "react";
 const Product = () => {
   return (
     <div className="flex flex-col h-full pb-10 min-h-[351] min-w-1/2 lg:min-w-0">
-      <Image
-        src="/main/drracaena_5.jpeg"
-        className="rounded-md flex-1"
-        height={400}
-        width={304}
-        alt="product-image"
-      />
-      <div className="px-1 py-0.5">
+      <div className="lg:size-[300] size-[160] sm:size-[220] md:size-[260] relative">
+        <Image
+          src="/main/drracaena_5.jpeg"
+          className="rounded-md object-cover"
+          fill
+          alt="product-image"
+        />
+      </div>
+      <div className="px-1 py-0.5 mt-2">
         <p>Golden Pothis</p>
         <span className="font-semibold">€10.00</span>
       </div>
