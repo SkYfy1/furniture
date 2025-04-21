3d13e2d193dca9f3ff26e8667b94953d067d044c
SkYfy1 <festik.tawer@gmail.com>
2025-04-22 01:02:47 +0300

    feature(lib) - debounce fn

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,15 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function debounce<T extends (...args: Parameters<T>) => void>(
+  callback: T,
+  ms: number
+) {
+  let timer: ReturnType<typeof setTimeout>;
+
+  return function (...args: Parameters<T>) {
+    clearTimeout(timer);
+    timer = setTimeout(() => callback(...args), ms);
+  };
+}
