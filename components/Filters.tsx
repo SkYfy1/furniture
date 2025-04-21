9b67a68e5ab42a0d185a9f71ca2f6416647e725f
SkYfy1 <festik.tawer@gmail.com>
2025-04-22 02:15:52 +0300

    feature(components/Filters) - responsiveness + search params reset

--- components/Filters.tsx
+++ components/Filters.tsx
@@ -2,12 +2,25 @@
 import React from "react";
 import CustomSlider from "./CustomSlider";
 import CustomSelect from "./CustomSelect";
+import { redirect, usePathname } from "next/navigation";
 
 const Filters: React.FC = () => {
+  const path = usePathname();
+  const handleClearParams = () => {
+    redirect(path);
+  };
   return (
-    <div className="flex flex-col gap-3 mb-10">
-      <CustomSelect />
-      <CustomSlider />
+    <div className="flex md:flex-row flex-col gap-3 mb-10">
+      <div className="flex gap-3 flex-col sm:flex-row">
+        <CustomSelect />
+        <CustomSlider />
+      </div>
+      <button
+        className="w-fit text-sm justify-start ml-1 mt-1"
+        onClick={handleClearParams}
+      >
+        Remove all filters
+      </button>
     </div>
   );
 };
