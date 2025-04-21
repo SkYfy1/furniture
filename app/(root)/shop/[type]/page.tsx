fe010d782d1df5d06e470cd8b727b0b3afa304ce
SkYfy1 <festik.tawer@gmail.com>
2025-04-22 01:37:46 +0300

    feature(app) - shop page products grid

--- app/(root)/shop/[type]/page.tsx
+++ app/(root)/shop/[type]/page.tsx
@@ -1,4 +1,6 @@
 import Filters from "@/components/Filters";
+import Product from "@/components/Product";
+import { Switch } from "@/components/ui/switch";
 import React from "react";
 
 interface Props {
@@ -17,6 +19,23 @@ const Page: React.FC<Props> = async ({ params, searchParams }) => {
         <h2 className="text-3xl font-bold py-3.5">{type}</h2>
         <p className="mb-10">{type}</p>
         <Filters />
+        <div className="text-sm font-semibold flex justify-between">
+          <div>Found 10 matching results</div>
+          <div className="flex gap-2.5 items-center">
+            <span>Show variants</span>
+            <Switch />
+          </div>
+        </div>
+        <div className="grid grid-cols-2 mt-5 gap-5 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5">
+          <Product />
+          <Product />
+          <Product />
+          <Product />
+          <Product />
+          <Product />
+          <Product />
+          <Product />
+        </div>
       </div>
     </>
   );
