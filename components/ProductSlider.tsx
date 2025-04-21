2c79cb8a92d8a7a27ef833d186111347b0eb14f9
SkYfy1 <festik.tawer@gmail.com>
2025-04-21 19:42:49 +0300

    refactor(components/ProductSlider.tsx) - brakepoints

--- components/ProductSlider.tsx
+++ components/ProductSlider.tsx
@@ -10,16 +10,16 @@ interface Props {
 
 const ProductSlider: React.FC<Props> = ({ title, text }) => {
   return (
-    <section className="container border-t border-gray-200 pt-18 lg:pb-32 px-4 lg:px-0">
-      <div className="flex flex-col md:flex-row justify-between items-center pb-10">
-        <div className="flex flex-col gap-2 self-start">
+    <section className="container border-t border-gray-200 pt-18 lg:pb-32 px-4 md:px-10 2xl:px-0">
+      <div className="flex flex-col lg:flex-row justify-between items-center pb-10">
+        <div className="flex flex-col gap-2 self-start lg:self-auto">
           <h2 className="font-semibold text-2xl">{title}</h2>
-          <p className="text-sm hidden md:block">{text}</p>
+          <p className="text-sm hidden xl:block">{text}</p>
         </div>
         <CustomLink
           title="View all products"
           link="/shop/plants"
-          className="w-full text-center mt-10"
+          className="w-full lg:w-fit text-center mt-10 lg:mt-0"
         />
       </div>
       <MotionList>
