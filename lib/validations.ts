9fdb1d6ef3468d7e36f25150289bebd8425ef089
SkYfy1 <festik.tawer@gmail.com>
2025-04-26 04:09:37 +0300

    feature(lib/validations) - zod userForm valid

---
+++ lib/validations.ts
@@ -0,0 +1,15 @@
+import { z } from "zod";
+
+export const signUpSchema = z.object({
+  email: z.string().email({ message: "Invalid email address" }),
+  name: z.string(),
+  password: z.string().min(6, "Password is too weak!"),
+});
+
+export const signInSchema = z.object({
+  email: z.string().email({ message: "Invalid email address" }),
+  password: z.string().min(6, "Password is too weak!"),
+});
+
+export type signInType = z.infer<typeof signInSchema>;
+export type signUpType = z.infer<typeof signUpSchema>;
