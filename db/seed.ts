033805c7ddfafbd0c1b739f3f39caae5c03cd16c
SkYfy1 <festik.tawer@gmail.com>
2025-04-29 00:49:40 +0300

    fix(db/seed) - removed db instance export

--- db/seed.ts
+++ db/seed.ts
@@ -8,7 +8,7 @@ config({ path: ".env.local" });
 
 const sql = neon(process.env.DATABASE_URL!);
 
-export const db = drizzle({ client: sql });
+const db = drizzle({ client: sql });
 
 const seedDb = async () => {
   console.log("Start!");
