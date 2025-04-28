a611a06237a67da0b2d2aaff76e9adbef4cd9328
SkYfy1 <festik.tawer@gmail.com>
2025-04-29 00:51:55 +0300

    chore(types) - define Product interface for Product component

--- types.d.ts
+++ types.d.ts
@@ -9,3 +9,23 @@ interface AuthCredentials {
   email: string;
   password: string;
 }
+
+interface Dimensions {
+  width: string;
+  height: string;
+  depth: string;
+  weight: string;
+}
+
+interface Product {
+  id: string;
+  name: string;
+  category: string;
+  description: string;
+  price: number;
+  imageUrl: string;
+  dimensions: Dimensions | unknown;
+  discount: number | null;
+  discountedPrice: number | null;
+  tags: string[] | null;
+}
