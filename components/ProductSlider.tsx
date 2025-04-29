307c22af489cdb491ecd98a21332892a9ba236d2
SkYfy1 <festik.tawer@gmail.com>
2025-04-30 01:21:03 +0300

    refactor(components/ProductSlider) - replace mock data with db

--- components/ProductSlider.tsx
+++ components/ProductSlider.tsx
@@ -2,19 +2,26 @@ import React from "react";
 import MotionList from "./MotionList";
 import Product from "./Product";
 import CustomLink from "./CustomLink";
+import { db } from "@/db/drizzle";
+import { productsTable } from "@/db/schema";
+import { eq } from "drizzle-orm";
 
 interface Props {
-  title: string;
-  text: string;
+  category: string;
+  description: string;
 }
 
-const ProductSlider: React.FC<Props> = ({ title, text }) => {
+const ProductSlider: React.FC<Props> = async ({ category, description }) => {
+  const products = await db
+    .select()
+    .from(productsTable)
+    .where(eq(productsTable.category, category));
   return (
     <section className="container border-t border-gray-200 pt-18 lg:pb-32 px-4 md:px-10 2xl:px-0">
       <div className="flex flex-col lg:flex-row justify-between items-center pb-10">
         <div className="flex flex-col gap-2 self-start lg:self-auto">
-          <h2 className="font-semibold text-2xl">{title}</h2>
-          <p className="text-sm hidden xl:block">{text}</p>
+          <h2 className="font-semibold text-2xl">{category}</h2>
+          <p className="text-sm hidden xl:block">{description}</p>
         </div>
         <CustomLink
           title="View all products"
@@ -23,11 +30,9 @@ const ProductSlider: React.FC<Props> = ({ title, text }) => {
         />
       </div>
       <MotionList>
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-        <Product />
+        {products.map((product) => (
+          <Product key={product.id} data={product} />
+        ))}
       </MotionList>
     </section>
   );
