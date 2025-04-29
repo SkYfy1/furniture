40916a6b328b38c3aaee193dddf95c80dc8d7266
SkYfy1 <festik.tawer@gmail.com>
2025-04-30 01:23:28 +0300

    refactor(shop/page) - categories array

--- app/(root)/shop/page.tsx
+++ app/(root)/shop/page.tsx
@@ -1,6 +1,7 @@
 import CustomLink from "@/components/CustomLink";
 import ProductSlider from "@/components/ProductSlider";
 import SaleOverview from "@/components/SaleOverview";
+import { categories } from "@/constants";
 import React from "react";
 
 const Page = () => {
@@ -10,26 +11,22 @@ const Page = () => {
       <div className="mt-20 mb-10 px-5 flex flex-col gap-4 container">
         <h2 className="text-sm font-semibold">Browse categories</h2>
         <div className="flex flex-wrap gap-1.5">
-          <CustomLink title="Flowers" link="/shop/flowers" />
-          <CustomLink title="Plants" link="/shop/plants" />
-          <CustomLink title="Kitchen" link="/shop/kitchen" />
-          <CustomLink title="Wall Art" link="/shop/wall-art" />
-          <CustomLink title="Sofas" link="/shop/sofas" />
-          <CustomLink title="Phones" link="/shop/phones" />
+          {categories.map((category) => (
+            <CustomLink
+              title={category.name}
+              link={category.route}
+              key={category.route}
+            />
+          ))}
         </div>
       </div>
-      <ProductSlider
-        title="Flowers"
-        text="Plants of all shapes and sizes for your home. With a little TLC, you’ll enjoy them for years to come."
-      />
-      <ProductSlider
-        title="Plants"
-        text="Plants of all shapes and sizes for your home. With a little TLC, you’ll enjoy them for years to come."
-      />
-      <ProductSlider
-        title="Kitchen"
-        text="Plants of all shapes and sizes for your home. With a little TLC, you’ll enjoy them for years to come."
-      />
+      {categories.map((category) => (
+        <ProductSlider
+          category={category.name}
+          description={category.desc}
+          key={category.name}
+        />
+      ))}
     </>
   );
 };
