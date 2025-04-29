92b14acdae79fd5def4221030acd8b6bee203dc0
SkYfy1 <festik.tawer@gmail.com>
2025-04-30 01:25:37 +0300

    refactor(shop/[type]/page) - replace mock data with db + added description

--- app/(root)/shop/[type]/page.tsx
+++ app/(root)/shop/[type]/page.tsx
@@ -1,6 +1,10 @@
 import Filters from "@/components/Filters";
 import Product from "@/components/Product";
 import { Switch } from "@/components/ui/switch";
+import { categories } from "@/constants";
+import { db } from "@/db/drizzle";
+import { productsTable } from "@/db/schema";
+import { eq } from "drizzle-orm";
 import React from "react";
 
 interface Props {
@@ -11,30 +15,33 @@ interface Props {
 const Page: React.FC<Props> = async ({ params, searchParams }) => {
   const type = (await params).type;
   const filters = await searchParams;
+  const category = type.charAt(0).toUpperCase() + type.slice(1);
+  const description = categories.find((el) => el.name === category)?.desc;
 
   console.log(filters);
+
+  const products = await db
+    .select()
+    .from(productsTable)
+    .where(eq(productsTable.category, category));
+
   return (
     <>
       <div className="container px-4 capitalize mb-10">
         <h2 className="text-3xl font-bold py-3.5">{type}</h2>
-        <p className="mb-10">{type}</p>
+        <p className="mb-10">{description}</p>
         <Filters />
         <div className="text-sm font-semibold flex justify-between">
-          <div>Found 10 matching results</div>
+          <div>Found {products.length} matching results</div>
           <div className="flex gap-2.5 items-center">
             <span>Show variants</span>
             <Switch />
           </div>
         </div>
         <div className="grid grid-cols-2 mt-5 gap-5 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5">
-          <Product />
-          <Product />
-          <Product />
-          <Product />
-          <Product />
-          <Product />
-          <Product />
-          <Product />
+          {products.map((product) => (
+            <Product key={product.id} data={product} />
+          ))}
         </div>
       </div>
     </>
