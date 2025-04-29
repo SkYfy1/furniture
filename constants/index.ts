60a78ebb383f11441d21b16ae6e4887f760982af
SkYfy1 <festik.tawer@gmail.com>
2025-04-30 01:13:34 +0300

    chore(constants) - categories array for link and sliders in shop page

---
+++ constants/index.ts
@@ -0,0 +1,23 @@
+export const categories = [
+  {
+    name: "Sofas",
+    desc: "Comfy, stylish, and affordable.",
+    route: "/shop/sofas",
+  },
+  {
+    name: "Plants",
+    desc: "Plants of all shapes and sizes for your home. With a little TLC, you’ll enjoy them for years to come.",
+    route: "/shop/plants",
+  },
+  {
+    name: "Flowers",
+    desc: "Most beautiful flowers in the world.",
+    route: "/shop/flowers",
+  },
+  {
+    name: "Kitchen",
+    desc: "Beautiful appliances that save you time and effort. Enjoy professional results from home.",
+    route: "/shop/kitchen",
+  },
+  { name: "Phones", desc: "Just phones.", route: "/shop/phones" },
+];
