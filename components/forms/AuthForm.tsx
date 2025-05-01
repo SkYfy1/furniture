e29af6814b875b351ccb8c7878000f22f53884e4
SkYfy1 <festik.tawer@gmail.com>
2025-05-02 02:32:09 +0300

    feature(AuthForm/Input) - implemented show/hide password button

--- components/forms/AuthForm.tsx
+++ components/forms/AuthForm.tsx
@@ -9,10 +9,11 @@ import {
   FieldError,
 } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
-import React from "react";
+import React, { useState } from "react";
 import { ZodType } from "zod";
 import { useRouter } from "next/navigation";
 import CustomInput from "./CustomInput";
+import Image from "next/image";
 
 interface Props<T extends FieldValues> {
   type: "SIGN_UP" | "SIGN_IN";
@@ -28,6 +29,7 @@ const AuthForm = <T extends FieldValues>({
   submitHandler,
 }: Props<T>) => {
   const router = useRouter();
+  const [showPass, setShowPass] = useState(false);
 
   const {
     register,
@@ -48,7 +50,10 @@ const AuthForm = <T extends FieldValues>({
     }
   };
   return (
-    <form onSubmit={handleSubmit(submitForm)} className="flex flex-col gap-4">
+    <form
+      onSubmit={handleSubmit(submitForm)}
+      className="flex flex-col gap-4 relative"
+    >
       <CustomInput<T>
         register={register}
         error={errors.email as FieldError}
@@ -65,6 +70,15 @@ const AuthForm = <T extends FieldValues>({
         register={register}
         error={errors.password as FieldError}
         name={"password" as Path<T>}
+        showPass={showPass}
+      />
+      <Image
+        onClick={() => setShowPass((prev) => !prev)}
+        className="absolute bottom-19 right-3"
+        src={showPass ? "/svg/eye-hide.svg" : "/svg/eye-show.svg"}
+        width={18}
+        height={18}
+        alt="Show password icon"
       />
       <button
         className="w-full bg-black text-white py-2 rounded-sm"
--- components/forms/CustomInput.tsx
+++ components/forms/CustomInput.tsx
@@ -10,12 +10,14 @@ interface Props<T extends FieldValues> {
   register: UseFormRegister<T>;
   error?: FieldError;
   name: Path<T>;
+  showPass?: boolean;
 }
 
 const CustomInput = <T extends FieldValues>({
   register,
   error,
   name,
+  showPass,
 }: Props<T>) => {
   return (
     <>
@@ -24,12 +26,21 @@ const CustomInput = <T extends FieldValues>({
         className="w-full px-4 py-1.5 bg-gray border border-gray focus-within:border-black rounded-sm"
       >
         <p className="text-sm font-semibold capitalize">{name}*</p>
-        <input
-          className="bg-gray-200/20 w-full rounded-xs focus:outline-none"
-          type="text"
-          id={name}
-          {...register(name as Path<T>)}
-        />
+        {name == "password" ? (
+          <input
+            className="bg-gray-200/20 w-full rounded-xs focus:outline-none"
+            type={showPass ? "text" : "password"}
+            id={name}
+            {...register(name as Path<T>)}
+          />
+        ) : (
+          <input
+            className="bg-gray-200/20 w-full rounded-xs focus:outline-none"
+            type="text"
+            id={name}
+            {...register(name as Path<T>)}
+          />
+        )}
       </label>
       {error && <span>{error.message}</span>}
     </>
