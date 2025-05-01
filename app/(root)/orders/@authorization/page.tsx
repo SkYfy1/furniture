51513e71ee3180fcddb5195692b6a99ad00d433e
SkYfy1 <festik.tawer@gmail.com>
2025-05-02 01:56:30 +0300

    style/refactor(ap/@auth) - form styles changed

--- app/(root)/orders/@authorization/page.tsx
+++ app/(root)/orders/@authorization/page.tsx
@@ -12,25 +12,38 @@ const Page = () => {
   };
 
   return (
-    <div className="h-full w-full flex flex-col justify-center items-center min-h-[70vh]">
-      <button onClick={handleChangeForm}>
-        {register ? "Register form" : "Login Form"}
-      </button>
-      {register ? (
-        <AuthForm
-          type="SIGN_UP"
-          defaultValues={{ name: "", email: "", password: "" }}
-          submitHandler={signUp}
-          schema={signUpSchema}
-        />
-      ) : (
-        <AuthForm
-          type="SIGN_IN"
-          defaultValues={{ email: "", password: "" }}
-          submitHandler={signInWithCredentials}
-          schema={signUpSchema}
-        />
-      )}
+    <div className="h-full w-full flex justify-center items-center min-h-[70vh]">
+      <div className="w-1/3 flex flex-col gap-0">
+        <div className="mb-8 flex items-center justify-between">
+          <div>
+            <h1 className="text-2xl font-semibold pb-2">
+              {register ? "Register" : "Login"}
+            </h1>
+            <p>We`ll send you a magick link.</p>
+          </div>
+          <button
+            className="bg-black text-white py-2 px-4 rounded-sm"
+            onClick={handleChangeForm}
+          >
+            {register ? "Sign Up" : "Sign In"}
+          </button>
+        </div>
+        {register ? (
+          <AuthForm
+            type="SIGN_UP"
+            defaultValues={{ name: "", email: "", password: "" }}
+            submitHandler={signUp}
+            schema={signUpSchema}
+          />
+        ) : (
+          <AuthForm
+            type="SIGN_IN"
+            defaultValues={{ email: "", password: "" }}
+            submitHandler={signInWithCredentials}
+            schema={signUpSchema}
+          />
+        )}
+      </div>
     </div>
   );
 };
