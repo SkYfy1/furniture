ccac7eb59d9331022292b35acc463647b87e5352
SkYfy1 <festik.tawer@gmail.com>
2025-05-02 02:06:07 +0300

    style(app/order) - layout + @auth page resp fix

--- app/(root)/orders/@authorization/page.tsx
+++ app/(root)/orders/@authorization/page.tsx
@@ -13,13 +13,13 @@ const Page = () => {
 
   return (
     <div className="h-full w-full flex justify-center items-center min-h-[70vh]">
-      <div className="w-1/3 flex flex-col gap-0">
-        <div className="mb-8 flex items-center justify-between">
+      <div className="lg:w-1/3 flex flex-col">
+        <div className="mb-8 flex items-center justify-between gap-2">
           <div>
-            <h1 className="text-2xl font-semibold pb-2">
+            <h1 className="md:text-2xl text-xl font-semibold pb-2">
               {register ? "Register" : "Login"}
             </h1>
-            <p>We`ll send you a magick link.</p>
+            <p className="text-sm">We`ll send you a magick link.</p>
           </div>
           <button
             className="bg-black text-white py-2 px-4 rounded-sm"
--- app/(root)/orders/layout.tsx
+++ app/(root)/orders/layout.tsx
@@ -9,7 +9,7 @@ const Layout: React.FC<{
   const session = await auth();
 
   return (
-    <div className="container pt-12 min-h-screen">
+    <div className="container pt-12 min-h-screen px-4 lg:px-0">
       <div className="flex justify-between">
         <h1 className="text-2xl font-semibold">Your orders</h1>
         {session?.user && <LogOut />}
