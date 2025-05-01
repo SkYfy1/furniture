5c982a0c55e018afd228068f6d715593f6f190de
SkYfy1 <festik.tawer@gmail.com>
2025-05-01 23:05:38 +0300

    fix(components/ProductSlider) - link fix

--- components/ProductSlider.tsx
+++ components/ProductSlider.tsx
@@ -16,6 +16,7 @@ const ProductSlider: React.FC<Props> = async ({ category, description }) => {
     .select()
     .from(productsTable)
     .where(eq(productsTable.category, category));
+
   return (
     <section className="container border-t border-gray-200 pt-18 lg:pb-32 px-4 md:px-10 2xl:px-0">
       <div className="flex flex-col lg:flex-row justify-between items-center pb-10">
@@ -25,7 +26,7 @@ const ProductSlider: React.FC<Props> = async ({ category, description }) => {
         </div>
         <CustomLink
           title="View all products"
-          link="/shop/plants"
+          link={`/shop/${category.toLowerCase()}`}
           className="w-full lg:w-fit text-center mt-10 lg:mt-0"
         />
       </div>
