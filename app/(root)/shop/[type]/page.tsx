9eb189a7e07af1f4a2ac9a79b255cfbdeef3ba0c
SkYfy1 <festik.tawer@gmail.com>
2025-05-09 02:54:19 +0300

    feature(app/shop) - products filtering

--- app/(root)/shop/[type]/page.tsx
+++ app/(root)/shop/[type]/page.tsx
@@ -5,17 +5,17 @@ import React from "react";
 
 interface Props {
   params: Promise<{ type: string }>;
-  searchParams?: Promise<{ min: string; max: string; orderBy: string }>;
+  searchParams?: Promise<{ min?: number; max?: number; orderBy?: string }>;
 }
 
 const Page: React.FC<Props> = async ({ params, searchParams }) => {
   const type = (await params).type;
-  const filters = await searchParams; // eslint-disable-line
+  const filters = await searchParams;
   const category = type.charAt(0).toUpperCase() + type.slice(1);
   const description = categories.find((el) => el.name === category)?.desc;
 
   const [products, variants] = await Promise.all([
-    getProductsByCategory(category),
+    getProductsByCategory(category, filters),
     getVariantsJoin(category),
   ]);
 
