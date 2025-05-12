26e1a42d39fe2ac2217690eb7d9bebeef8dc1f3b
SkYfy1 <festik.tawer@gmail.com>
2025-05-13 00:55:27 +0300

    feature(app-layout/components) - add SearchOverlay component for search input [SearchProducts component]

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,3 +1,4 @@
+import BlackCover from "@/components/SearchOverlay";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import React, { ReactNode } from "react";
@@ -6,6 +7,7 @@ const Layout = ({ children }: { children: ReactNode }) => {
   return (
     <div className="root-container">
       <div className="pt-2 pb-2 text-black flex flex-col min-h-screen">
+        <BlackCover />
         {/* <div className="pt-2 pb-2 text-black "> */}
         <Header />
         <main className="mt-10 md:mt-18 flex-1">{children}</main>
---
+++ components/SearchOverlay.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import React, { useEffect } from "react";
+
+const SearchOverlay = () => {
+  const params = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+  const query = params.get("query") || "";
+  const closeSearch = () => {
+    const newParams = new URLSearchParams(params);
+
+    newParams.delete("query");
+
+    replace(`${pathname}?${newParams.toString()}`);
+  };
+
+  useEffect(() => {
+    const body = document.querySelector("body");
+    if (body && query) {
+      body.style.overflow = "hidden";
+    }
+
+    return () => {
+      if (body) body.style.overflow = "auto";
+    };
+  }, [query]);
+  if (!query) return null;
+  return (
+    <div
+      className="top-0 left-0 h-full w-screen absolute bg-black/75 z-50"
+      onClick={closeSearch}
+    ></div>
+  );
+};
+
+export default SearchOverlay;
