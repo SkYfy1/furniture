f2413cfa177cd473f363b8fcf4d565c29b06bc95
SkYfy1 <festik.tawer@gmail.com>
2025-05-12 16:31:52 +0300

    refactor(components/QueryProducts) - change ssr to client side -> fetch with useEffect

--- components/QueryProducts.tsx
+++ components/QueryProducts.tsx
@@ -1,14 +1,36 @@
-import { getQueryProduct } from "@/lib/data/products";
-import React from "react";
-import Product from "./Product";
+import React, { useEffect, useState } from "react";
+import ProductLink from "./ProductLink";
 
-const QueryProducts: React.FC<{ query: string }> = async ({ query }) => {
-  const products = await getQueryProduct(query);
-  console.log(products);
+const QueryProducts: React.FC<{ query?: string }> = ({ query }) => {
+  const [products, setProducts] = useState<Product[]>([]);
+  useEffect(() => {
+    let abort = false;
+    let timer: NodeJS.Timeout;
+
+    const fetchProds = async () => {
+      try {
+        const response = await fetch(`/api/search/${query}`);
+        const items = await response.json();
+        if (!abort) setProducts(items);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    timer = setTimeout(fetchProds, 100); // eslint-disable-line
+
+    return () => {
+      abort = true;
+      clearTimeout(timer);
+    };
+  }, [query]);
+
+  if (!products.length) return null;
+
   return (
-    <div className="absolute bg-white p-5 rounded-md top-13 left-70 z-50 flex flex-col gap-2">
+    <div className="absolute bg-white p-5 rounded-md top-15 min-w-[15vw] -left-4 z-50 flex flex-col gap-2">
       {products.map((item) => (
-        <Product key={item.id} data={item} name={item.name} />
+        <ProductLink key={item.id} item={item} />
       ))}
     </div>
   );
