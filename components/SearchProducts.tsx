6cbd88bec40a8018fe2d05e8990f5b8eb8809493
SkYfy1 <festik.tawer@gmail.com>
2025-05-12 16:28:23 +0300

    fix(components/SearchProducts) - removed state -> uncontrolled input -> query handling only in params

--- components/SearchProducts.tsx
+++ components/SearchProducts.tsx
@@ -1,13 +1,14 @@
 import { debounce } from "@/lib/utils";
 import Image from "next/image";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
-import React, { useCallback, useState } from "react";
+import React, { useCallback } from "react";
+import QueryProducts from "./QueryProducts";
 
 const SearchProducts: React.FC = () => {
-  const [text, setText] = useState("");
   const params = useSearchParams();
   const path = usePathname();
   const { replace } = useRouter();
+  const query = params.get("query") || "";
 
   const updateQuery = useCallback(
     debounce((term: string) => {
@@ -19,13 +20,12 @@ const SearchProducts: React.FC = () => {
         searchParams.delete("query");
       }
 
-      replace(`${path}/?${searchParams.toString()}`);
+      replace(`${path}?${searchParams.toString()}`);
     }, 300),
     [path, params]
   );
 
   const handleChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setText(e.target.value);
     updateQuery(e.target.value);
   };
 
@@ -33,7 +33,6 @@ const SearchProducts: React.FC = () => {
     <div className="relative">
       <input
         type="text"
-        value={text}
         onChange={handleChangeInput}
         placeholder="Names, categories..."
         className="bg-gray-50 rounded-full pl-6 pr-4 xl:pr-22 py-2 focus:outline-1"
@@ -45,6 +44,7 @@ const SearchProducts: React.FC = () => {
         width={18}
         alt="search-icon"
       />
+      {query && <QueryProducts query={params.get("query")?.toString()} />}
     </div>
   );
 };
