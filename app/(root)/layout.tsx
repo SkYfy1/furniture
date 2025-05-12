14d9c72b7a8761293ab45850f73fbd0f508e13d0
SkYfy1 <festik.tawer@gmail.com>
2025-05-13 01:15:07 +0300

    fix(app/layout) - import SearchOverlay fix (BlackCover)

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,4 +1,4 @@
-import BlackCover from "@/components/SearchOverlay";
+import SearchOverlay from "@/components/SearchOverlay";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import React, { ReactNode } from "react";
@@ -7,8 +7,7 @@ const Layout = ({ children }: { children: ReactNode }) => {
   return (
     <div className="root-container">
       <div className="pt-2 pb-2 text-black flex flex-col min-h-screen">
-        <BlackCover />
-        {/* <div className="pt-2 pb-2 text-black "> */}
+        <SearchOverlay />
         <Header />
         <main className="mt-10 md:mt-18 flex-1">{children}</main>
         <Footer />
