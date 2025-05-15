90b2160a7f8f78f039352b283cdd097c6e6082f7
SkYfy1 <festik.tawer@gmail.com>
2025-05-16 00:46:03 +0300

    feature(lib/validations) - zod orderSchema for order form validation

--- lib/validations.ts
+++ lib/validations.ts
@@ -11,5 +11,24 @@ export const signInSchema = z.object({
   password: z.string().min(6, "Password is too weak!"),
 });
 
+export const orderSchema = z.object({
+  firstName: z.string().min(2),
+  lastName: z.string().min(2),
+  country: z.string().min(2),
+  city: z.string().min(2),
+  state: z.string().min(2),
+  zip: z.string().regex(/^\d{5}$/, {
+    message: "Zip code must contain 5 digits",
+  }),
+  address: z.string().min(2),
+  shippingService: z.enum(["MEEST", "NOVAPOST", "UKRPOSTA"], {
+    message: "Please choose either MEEST, NOVAPOST or URKPOSTA",
+  }),
+  paymentMethod: z.enum(["CASH", "CARD", "CRYPTO"], {
+    message: "Please choose either CARD, CASH or CRYPTO",
+  }),
+});
+
+export type orderType = z.infer<typeof orderSchema>;
 export type signInType = z.infer<typeof signInSchema>;
 export type signUpType = z.infer<typeof signUpSchema>;
