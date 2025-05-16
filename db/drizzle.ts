b1a6957eb360446ee9f3b83b3bee34134c6d84a6
SkYfy1 <festik.tawer@gmail.com>
2025-05-17 00:53:57 +0300

    fix(db) - migrate to WS driver for transaction support

--- db/drizzle.ts
+++ db/drizzle.ts
@@ -1,6 +1,7 @@
-import { drizzle } from "drizzle-orm/neon-http";
-import { neon } from "@neondatabase/serverless";
+import { neonConfig, Pool } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-serverless";
+import { WebSocket } from "ws";
 
-const sql = neon(process.env.DATABASE_URL!);
-
-export const db = drizzle({ client: sql });
+neonConfig.webSocketConstructor = WebSocket;
+const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+export const db = drizzle(pool);
