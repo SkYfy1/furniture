a99ea229b0d5030681783726f9b2bafb1e40c16c
SkYfy1 <festik.tawer@gmail.com>
2025-05-19 01:27:08 +0300

    fix(app/OrdersPage) - add return statement + Order Components props

--- app/(root)/orders/@orders/page.tsx
+++ app/(root)/orders/@orders/page.tsx
@@ -1,5 +1,9 @@
 import { auth } from "@/auth";
-import { getOrderData } from "@/lib/data/order";
+import {
+  getOrderData,
+  getPaymentInfo,
+  getShippingInfo,
+} from "@/lib/data/order";
 import React from "react";
 import Order from "./components/Order";
 import Link from "next/link";
@@ -12,16 +16,25 @@ const Page = async () => {
   const orders = await getOrderData(id);
 
   if (!orders.length) {
-    <div className="flex justify-center items-center flex-col gap-2">
-      <h1>No Orders Placed Yet</h1>
-      <Link href="/shop">Continue shopping</Link>
-    </div>;
+    return (
+      <div className="flex items-center pt-36 flex-col gap-4 text-black">
+        <h1 className="text-5xl font-semibold">No Orders Placed Yet</h1>
+        <Link href="/shop" className="underline text-sm">
+          Continue shopping
+        </Link>
+      </div>
+    );
   }
 
   return (
-    <div className="p-4 mt-8 flex flex-col gap-3">
+    <div className="p-4 mt-8 flex flex-col gap-3 bg-white rounded-md">
       {orders.map((order) => (
-        <Order key={order.order.id} order={order} />
+        <Order
+          key={order.order.id}
+          order={order}
+          getOrderDelivery={getShippingInfo(order.order.shippingInfo)}
+          getOrderPayment={getPaymentInfo(order.order.paymentInfo)}
+        />
       ))}
     </div>
   );
