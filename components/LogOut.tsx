eb129ac9a5a505f566d1643c76c43b9500a7f1a1
SkYfy1 <festik.tawer@gmail.com>
2025-05-19 01:12:53 +0300

    fix(components/LogOut) - cursor pointer fix

--- components/LogOut.tsx
+++ components/LogOut.tsx
@@ -4,7 +4,7 @@ import React from "react";
 const LogOut = () => {
   return (
     <button
-      className="bg-gray px-2.5 py-2 rounded-sm hover:bg-black hover:text-white duration-250 text-xs flex items-center gap-1"
+      className="bg-gray px-2.5 py-2 rounded-sm hover:bg-black hover:text-white duration-250 text-xs flex items-center gap-1 cursor-pointer"
       onClick={singOut}
     >
       Log out
