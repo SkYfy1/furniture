cbaa66433d7b9bb3c626adf29416f716227668c0
SkYfy1 <festik.tawer@gmail.com>
2025-05-19 01:36:20 +0300

    feature(app/receipt) - fake receipt page

---
+++ app/(root)/receipt/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Page = () => {
+  return (
+    <div className="container text-center mt-12">
+      <h1 className="text-5xl font-semibold block h-full">
+        Fake receipt route
+      </h1>
+    </div>
+  );
+};
+
+export default Page;
