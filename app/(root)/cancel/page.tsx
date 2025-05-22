40dc3c270fe12d42d5218e2aaed831f525cfcf94
SkYfy1 <festik.tawer@gmail.com>
2025-05-22 23:46:56 +0300

    feature(app/Cancel) - rejected payment page

---
+++ app/(root)/cancel/page.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Page = () => {
+  return (
+    <div className="container text-center mt-8 lg:mt-16">
+      <h1 className="text-lg lg:text-5xl font-semibold block h-full">
+        There was an issue processing your payment.
+      </h1>
+      <p className="mt-4 text-sm lg:text-base">
+        Try again later in orders page!
+      </p>
+    </div>
+  );
+};
+
+export default Page;
