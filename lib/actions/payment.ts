3f655d0386cce87a94524e6d56bc43e5629785fd
SkYfy1 <festik.tawer@gmail.com>
2025-05-24 02:35:12 +0300

    feature(lib/Payments) - buy now action (stripe form)

--- lib/actions/payment.ts
+++ lib/actions/payment.ts
@@ -3,6 +3,8 @@
 import { Products, Variants } from "@/db/tableTypes";
 import { stripe } from "../stripe";
 import { createLineItems, isProductTable } from "../utils";
+import { sql } from "drizzle-orm";
+import { db } from "@/db/drizzle";
 
 export const retryPayment = async ({
   userId,
@@ -13,39 +15,155 @@ export const retryPayment = async ({
   paymentId: string;
   orderedProducts: Variants[] | Products[];
 }) => {
-  // Filter orderedProducts Array
-  const { variantsArray, productsArray } = orderedProducts.reduce<{
-    variantsArray: Variants[];
-    productsArray: Products[];
-  }>(
-    (acc, cur) => {
-      if (isProductTable(cur)) {
-        acc.productsArray.push(cur);
-      } else {
-        acc.variantsArray.push(cur);
-      }
+  try {
+    // Filter orderedProducts Array
+    const { variantsArray, productsArray } = orderedProducts.reduce<{
+      variantsArray: Variants[];
+      productsArray: Products[];
+    }>(
+      (acc, cur) => {
+        if (isProductTable(cur)) {
+          acc.productsArray.push(cur);
+        } else {
+          acc.variantsArray.push(cur);
+        }
 
-      return acc;
-    },
-    { variantsArray: [], productsArray: [] }
-  );
-  //   Create line_items for session
-  const line_items = createLineItems(variantsArray, productsArray);
+        return acc;
+      },
+      { variantsArray: [], productsArray: [] }
+    );
+    //   Create line_items for session
+    const line_items = createLineItems(variantsArray, productsArray);
 
-  //   Create session
-  const session = await stripe.checkout.sessions.create({
-    payment_method_types: ["card"],
-    line_items,
-    mode: "payment",
-    success_url: `${process.env.NEXT_PUBLIC_URL}/success?payment_id=${paymentId}`,
-    cancel_url: `${process.env.NEXT_PUBLIC_URL}/cancel`,
-    metadata: {
-      payment_id: paymentId,
-      user_id: userId,
-    },
-  });
+    //   Create session
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ["card"],
+      line_items,
+      mode: "payment",
+      success_url: `${process.env.NEXT_PUBLIC_URL}/success?payment_id=${paymentId}`,
+      cancel_url: `${process.env.NEXT_PUBLIC_URL}/cancel`,
+      metadata: {
+        payment_id: paymentId,
+        user_id: userId,
+      },
+    });
 
-  //   Return session URL
+    //   Return session URL
 
-  return session.url;
+    return session.url;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const buyNow = async ({
+  id,
+  name,
+  quantity,
+  price,
+  imageUrl,
+  type,
+  userId,
+}: {
+  id: string;
+  name: string;
+  quantity: number;
+  price: number;
+  imageUrl: string;
+  type: string;
+  userId: string;
+}) => {
+  try {
+    const productData = { id, name, quantity, price, imageUrl, type };
+    const {
+      rows: [{ gen_random_uuid: paymentId }],
+    } = await db.execute(sql`SELECT gen_random_uuid()`);
+
+    const line_item = {
+      price_data: {
+        currency: "eur",
+        product_data: {
+          name: name as string,
+          images: [imageUrl as string],
+        },
+        unit_amount: price * 100,
+      },
+      quantity: quantity,
+    };
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ["card"],
+      line_items: [line_item],
+      mode: "payment",
+      success_url: `${process.env.NEXT_PUBLIC_URL}/success?payment_id=${paymentId}`,
+      cancel_url: `${process.env.NEXT_PUBLIC_URL}/cancel`,
+      billing_address_collection: "required",
+      shipping_address_collection: {
+        allowed_countries: ["UA", "FR", "DE", "IT", "PL", "CZ", "US", "GB"],
+      },
+      phone_number_collection: {
+        enabled: true,
+      },
+      custom_fields: [
+        {
+          key: "shipping_service",
+          label: {
+            type: "custom",
+            custom: "Shipping service",
+          },
+          type: "dropdown",
+          dropdown: {
+            options: [
+              {
+                label: "NOVAPOST",
+                value: "NOVAPOST",
+              },
+              {
+                label: "MEEST",
+                value: "MEEST",
+              },
+              {
+                label: "UKRPOSTA",
+                value: "UKRPOSTA",
+              },
+            ],
+          },
+        },
+        {
+          key: "payment_method",
+          label: {
+            type: "custom",
+            custom: "Payment method",
+          },
+          type: "dropdown",
+          dropdown: {
+            options: [
+              {
+                label: "CASH",
+                value: "CASH",
+              },
+              {
+                label: "CARD",
+                value: "CARD",
+              },
+              {
+                label: "CRYPTO",
+                value: "CRYPTO",
+              },
+            ],
+          },
+        },
+      ],
+      metadata: {
+        payment_id: paymentId as string,
+        client_id: userId,
+        type: "create",
+        productData: JSON.stringify(productData),
+      },
+    });
+
+    return { success: true, message: "Success", url: session.url };
+  } catch (error) {
+    console.log(error);
+    return { success: false, message: "Error, please try again later." };
+  }
 };
