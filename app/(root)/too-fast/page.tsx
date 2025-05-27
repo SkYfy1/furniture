a539bcb151becfdd7c5df91969f74173655811d2
SkYfy1 <festik.tawer@gmail.com>
2025-05-28 01:57:18 +0300

    feature(app/too-fast) - rate limit too many request page

---
+++ app/(root)/too-fast/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Page = () => {
+  return (
+    <div className="container mt-12 text-center">
+      <h1 className="text-5xl font-semibold block h-full">Too fast</h1>
+    </div>
+  );
+};
+
+export default Page;
