303a2ba4ff001593ddc891b36f7b70c481ea5f2d
SkYfy1 <festik.tawer@gmail.com>
2025-05-29 01:08:14 +0300

    feature(seo) - add metadata for /shop/type

--- app/(root)/shop/[type]/page.tsx
+++ app/(root)/shop/[type]/page.tsx
@@ -4,11 +4,25 @@ import {
   getFilteredVariants,
   getProductsByCategory,
 } from "@/lib/data/products";
+import { Metadata } from "next";
 import React from "react";
 
 interface Props {
   params: Promise<{ type: string }>;
   searchParams?: Promise<{ min?: number; max?: number; orderBy?: string }>;
+}
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const type = (await params).type;
+  const title = type.slice(0, 1).toUpperCase() + type.slice(1);
+  return {
+    title: title,
+    openGraph: {
+      url: type,
+      title: `FRNTR | ${title}`,
+      description: "Incredible deals. Top-of-the-line design for less.",
+    },
+  };
 }
 
 const Page: React.FC<Props> = async ({ params, searchParams }) => {
