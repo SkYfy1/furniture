109d3f4fda3ec40b7ef3c2eac5e306bc0af3b0fc
SkYfy1 <festik.tawer@gmail.com>
2025-05-31 01:47:19 +0300

    feature(app-i18n) - add en/fr translations for Main Page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -6,12 +6,14 @@ import {
   getOneProductFromThreeCategories,
   getProductsByCategory,
 } from "@/lib/data/products";
+import { getTranslations } from "next-intl/server";
 
 export default async function Home() {
   const [plants, outlet] = await Promise.all([
     getProductsByCategory("Plants"),
     getOneProductFromThreeCategories(),
   ]);
+  const t = await getTranslations("HomePage");
 
   return (
     <div className="grid-cols-custom grid lg:gap-4 lg:gap-y-5 gap-1 lg:my-5">
@@ -20,11 +22,9 @@ export default async function Home() {
       <section className="lg:pt-24 pt-20 col-start-2 col-end-5 h-full w-full flex-col flex gap-8">
         <div className="flex gap-2 flex-col">
           <h1 className="lg:text-3xl text-2xl font-bold">
-            To Plant a Garden is To Believe in Tomorrow
+            {t("Slider.title")}
           </h1>
-          <p className="text-sm lg:text-base">
-            Our plants are sure to brighten up your home (and your future).
-          </p>
+          <p className="text-sm lg:text-base">{t("Slider.subTitle")}</p>
         </div>
         <MotionList>
           {plants.map((plant) => (
@@ -39,8 +39,8 @@ export default async function Home() {
       </section>
       <section className="w-full bg-green-50 pt-24 col-span-5 px-8 lg:px-0">
         <div className="container pb-10">
-          <h1 className="text-3xl font-bold">Outlet</h1>
-          <p>Enjoy discounts on the following:</p>
+          <h1 className="text-3xl font-bold">{t("Outlet.title")}</h1>
+          <p>{t("Outlet.subTitle")}</p>
         </div>
         <div className="container">
           <MotionList className="gap-3 justify-start">
