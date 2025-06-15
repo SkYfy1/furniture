e9b2ff14fec418c7f888f202cc4639dc182c113a
SkYfy1 <festik.tawer@gmail.com>
2025-06-16 00:39:18 +0300

    chore(db/drizzle) - removed comments

--- db/drizzle.ts
+++ db/drizzle.ts
@@ -1,22 +1,3 @@
-// import { drizzle } from "drizzle-orm/neon-http";
-// import { neon } from "@neondatabase/serverless";
-
-// const sql = neon(process.env.DATABASE_URL!);
-
-// export const db = drizzle({ client: sql });
-
-//
-//
-//
-
-// import { neonConfig, Pool } from "@neondatabase/serverless";
-// import { drizzle } from "drizzle-orm/neon-serverless";
-// import { WebSocket } from "ws";
-
-// neonConfig.webSocketConstructor = WebSocket;
-// const pool = new Pool({ connectionString: process.env.DATABASE_URL });
-// export const db = drizzle(pool);
-
 import { Pool } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-serverless";
 
